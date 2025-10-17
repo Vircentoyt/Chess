0ec0611747ddca2154b5e35ac7be6c5ecc098c65
Matthias Nilsson <112242026+RalfiSlask@users.noreply.github.com>
2025-10-17 12:06:42 +0200

    init commit

---
+++ main_old.js
@@ -0,0 +1,51 @@
+let age = 13;
+
+console.log("This is Vincent's age:", age)
+
+age += 82;
+
+console.log("This is Vincent's age after we have changed it:", age)
+
+const matthiasAge = 37; // this is called a camelCase
+
+/* 
+    There are multiple types of variables. 
+    1. number
+    2. string
+    3. boolean (true/false)
+    4. null (the developer can set null by themself: Item does not exist)
+    5. undefined (does not exist)
+*/
+
+let name;
+
+console.log("is named defined:", name)
+
+let isMatthiasFat = false;
+
+console.log("is Matthias fat:", isMatthiasFat)
+
+isMatthiasFat = true
+
+console.log("Is Matthias fat after eating a hamburgir?", isMatthiasFat)
+
+console.log(typeof(name))
+
+let numberOfApples = 12
+let numberOfOranges = 93
+let amountOfFruits = numberOfApples + numberOfOranges 
+
+/* 
+    greater than > 
+    lesser than <
+    greater or equal to >=
+        lesser or equal to <=
+*/
+
+console.log(12>8)
+
+console.log(12<8)
+
+console.log(8 >= 8);
+
+console.log(4339 <= 49)
